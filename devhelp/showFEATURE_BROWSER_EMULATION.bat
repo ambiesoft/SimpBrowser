e22c49434b1d79a1ab2003d9e80d41cbc7a9432e
ambiesoft <ambiesoft.trueff@gmail.com>
2019-02-24 14:58:09 +0900

    show help tool ( View registory )

---
+++ devhelp/showFEATURE_BROWSER_EMULATION.bat
@@ -0,0 +1,4 @@
+
+
+powershell -Command "Start-Process C:\LegacyPrograms\SysinternalsSuite\regjump.exe \"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION\" -Verb RunAs"
+
